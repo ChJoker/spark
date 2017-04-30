dcff888a79a5681e3371276b39d0be987ede7aaf
xuyongliang <xuyongliang@linkingcloud.com>
2017-04-30 11:15:45 +0800

    init spark

---
+++ src/main/scala/Analysis.scala
@@ -0,0 +1,87 @@
+import org.apache.spark.sql.SparkSession
+
+
+object Analysis {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .getOrCreate()
+    val flightData = spark.read.option("header", "true").csv("/Users/apple/.ivy/jars/design/2007.csv")
+    flightData.createOrReplaceTempView("flights")
+
+    val airportData = spark.read.option("header", "true").csv("/Users/apple/.ivy/jars/design/airports-csv.csv")
+    airportData.createOrReplaceTempView("airports")
+
+    // 每天航班最繁忙的时间段是哪些？
+    val FlightNum_group_hours = spark.sql("SELECT count(*),floor(DepTime/100) FROM flights GROUP BY floor(DepTime/100) ORDER BY floor(DepTime/100) ")
+    val FlightNum_group_origin_hours = spark.sql("" +
+      " SELECT count(*),origin,floor(DepTime/100)" +
+      " FROM flights" +
+      " GROUP BY origin, floor(DepTime/100)" +
+      " ORDER BY origin, floor(DepTime/100)")
+    val FlightNum_group_day_hours = spark.sql("" +
+      " SELECT count(*),printf('%04d%02d%02d',cast(Year as int), cast(Month as int), cast(DayofMonth as int)),floor(DepTime/100)" +
+      " FROM flights" +
+      " GROUP BY printf('%04d%02d%02d',cast(Year as int), cast(Month as int), cast(DayofMonth as int)), floor(DepTime/100)" +
+      " ORDER BY printf('%04d%02d%02d',cast(Year as int), cast(Month as int), cast(DayofMonth as int)), floor(DepTime/100)")
+    val FlightNum_group_month_hours = spark.sql("" +
+      " SELECT count(*),printf('%04d%02d',cast(Year as int), cast(Month as int)),floor(DepTime/100)" +
+      " FROM flights" +
+      " GROUP BY printf('%04d%02d',cast(Year as int), cast(Month as int)), floor(DepTime/100)" +
+      " ORDER BY printf('%04d%02d',cast(Year as int), cast(Month as int)), floor(DepTime/100)")
+
+    // 出发延误的重灾区都有哪些？
+    val delay_group_origin = spark.sql("" +
+      " SELECT sum(DepDelay)/count(*) as percentage,origin" +
+      " FROM flights" +
+      " WHERE Cancelled = 0" +
+      " GROUP BY origin" +
+      " ORDER BY percentage DESC")
+
+    // 飞哪最准时
+    val not_delay_group_state = spark.sql("" +
+      " SELECT sum(ontimeArr)/sum(allArr) as percentage, state" +
+      " FROM (SELECT Dest, SUM(if(ArrDelay == 1, 0, 1)) as ontimeArr, COUNT(*) as allArr FROM flights WHERE Cancelled = 0 GROUP BY Dest) a" +
+      " JOIN airports b ON a.Dest = b.iata" +
+      " GROUP BY state" +
+      " ORDER BY percentage DESC")
+
+    // 什么时候是一年中最好的一天的飞行时间，以尽量减少延误？
+    val not_delay_group_day = spark.sql("" +
+      " SELECT sum(if(DepDelay == 1, 0,1))/count(*) as percentage,printf('%04d%02d%02d',cast(Year as int), cast(Month as int), cast(DayofMonth as int))" +
+      " FROM flights" +
+      " WHERE Cancelled = 0" +
+      " GROUP BY printf('%04d%02d%02d',cast(Year as int), cast(Month as int), cast(DayofMonth as int))" +
+      " ORDER BY percentage DESC")
+
+    // 不同地点之间飞行的人数会随时间而变化吗？
+    val flight_count_group_origin_day = spark.sql("" +
+      " SELECT count(*) as percentage,printf('%04d%02d%02d',cast(Year as int), cast(Month as int), cast(DayofMonth as int)) as date, origin" +
+      " FROM flights" +
+      " GROUP BY printf('%04d%02d%02d',cast(Year as int), cast(Month as int), cast(DayofMonth as int)), origin" +
+      " ORDER BY date")
+    val flight_count_group_Dest_day = spark.sql("" +
+      " SELECT count(*) as percentage,printf('%04d%02d%02d',cast(Year as int), cast(Month as int), cast(DayofMonth as int)) as date, dest" +
+      " FROM flights" +
+      " GROUP BY printf('%04d%02d%02d',cast(Year as int), cast(Month as int), cast(DayofMonth as int)), dest" +
+      " ORDER BY date")
+
+    // 天气如何影响飞机延误？
+    val weather_delay_group_origin = spark.sql("" +
+      " SELECT sum(if(WeatherDelay != 'NA', 1, 0))/count(*) as percentage, origin" +
+      " FROM flights" +
+      " WHERE DepDelay = 1" +
+      " GROUP BY origin" +
+      " ORDER BY percentage DESC")
+
+    // 级联故障
+    val delay_relate_group_airport = spark.sql("" +
+      " SELECT delaySum/lDelaySum as percentage, Origin" +
+      " FROM (SELECT year, month, dayofMonth, dest, COUNT(*) as delaySum FROM flights WHERE ArrDelay = 1 GROUP BY Dest, year, month, dayofMonth) a" +
+      " JOIN (SELECT year, month, dayofMonth, Origin, COUNT(*) as lDelaySum FROM flights WHERE LateAircraftDelay != 'NA' GROUP BY Origin, year, month, dayofMonth) b ON a.Dest = b.Origin and a.year = b.year and a.month = b.month and a.dayofMonth = b.dayofMonth" +
+      " ORDER BY percentage DESC")
+
+//    val r = delay_relate_group_airport.take(10)
+    delay_relate_group_airport.foreach(t => println(t))
+  }
+}
---
+++ src/main/scala/Predict.scala
@@ -0,0 +1,155 @@
+import org.apache.spark.sql.SparkSession
+
+import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.util.MLUtils
+import org.apache.spark.mllib.linalg.Vectors
+import org.apache.spark.mllib.regression.LabeledPoint
+import org.apache.spark.mllib.tree.DecisionTree
+import org.apache.spark.mllib.tree.model.DecisionTreeModel
+
+object Predict {
+  def main(args: Array[String]): Unit = {
+    val spark = SparkSession
+      .builder()
+      .getOrCreate()
+    val flightData = spark.read.option("header", "true").csv("/Users/apple/.ivy/jars/design/2007.csv")
+    val tmpFlightDataRDD = flightData.map(
+      row => row(2).toString + "," // dayOfMonth
+        + row(3).toString + "," // dayOfWeek
+        + row(5).toString + "," // CRSDepTime
+        + row(7).toString + "," // CRSArrTime
+        + row(8).toString + "," // UniqueCarrier
+        + row(12).toString + "," // CRSElapsedTime
+        + row(16).toString + "," // Origin
+        + row(17).toString + "," // Dest
+        + row(14).toString + "," // ArrDelay
+        + row(15).toString) // DepDelay
+    val flightRDD = tmpFlightDataRDD.map(parseFields)
+    var uniqueCarrier_id: Int = 0
+
+    var mCarrier: Map[String, Int] = Map()
+    flightRDD.map(flight => flight.uniqueCarrier).distinct.collect.foreach(x => {
+      mCarrier += (x -> uniqueCarrier_id);
+      uniqueCarrier_id += 1
+    })
+
+    var origin_id: Int = 0
+    var mOrigin: Map[String, Int] = Map()
+    flightRDD.map(flight => flight.origin).distinct.collect.foreach(x => {
+      mOrigin += (x -> origin_id);
+      origin_id += 1
+    })
+
+    var dest_id: Int = 0
+    var mDest: Map[String, Int] = Map()
+    flightRDD.map(flight => flight.dest).distinct.collect.foreach(x => {
+      mDest += (x -> dest_id);
+      dest_id += 1
+    })
+
+    val featuredRDD = flightRDD.map(flight => {
+      val vDayOfMonth = flight.dayOfMonth - 1
+      val vDayOfWeek = flight.dayOfWeek - 1
+      val vCRSDepTime = flight.crsDepTime
+      val vCRSArrTime = flight.crsArrTime
+      val vCarrierID = mCarrier(flight.uniqueCarrier)
+      val vCRSElapsedTime = flight.crsElapsedTime
+      val vOriginID = mOrigin(flight.origin)
+      val vDestID = mDest(flight.dest)
+      val vDelayFlag = flight.delayFlag
+
+      Array(vDelayFlag.toDouble, vDayOfMonth.toDouble, vDayOfWeek.toDouble, vCRSDepTime.toDouble, vCRSArrTime.toDouble, vCarrierID.toDouble, vCRSElapsedTime.toDouble, vOriginID.toDouble, vDestID.toDouble)
+    })
+
+    val LabeledRDD = featuredRDD.map(x => LabeledPoint(x(0), Vectors.dense(x(1), x(2), x(3), x(4), x(5), x(6), x(7), x(8))))
+
+    val notDelayedFlights = LabeledRDD.filter(x => x.label == 0).randomSplit(Array(0.8, 0.2))(1)
+    val delayedFlights = LabeledRDD.filter(x => x.label == 1)
+    val tmpTTData = notDelayedFlights.union(delayedFlights)
+    val TTData = tmpTTData.randomSplit(Array(0.7, 0.3))
+    val trainingData = TTData(0)
+    val testData = TTData(1)
+
+    // 仿照 API 文档中的提示，构造各项参数
+    var paramCateFeaturesInfo = Map[Int, Int]()
+
+    // 第一个特征信息：下标为 0 ，表示 dayOfMonth 有 0 到 30 的取值。
+    paramCateFeaturesInfo += (0 -> 31)
+
+    // 第二个特征信息：下标为 1 ，表示 dayOfWeek 有 0 到 6 的取值。
+    paramCateFeaturesInfo += (1 -> 7)
+
+    // 第三、四个特征是出发和抵达时间，这里我们不会用到，故省略。
+
+    // 第五个特征信息：下标为 4 ，表示 uniqueCarrier 的所有取值。
+    paramCateFeaturesInfo += (4 -> mCarrier.size)
+
+    // 第六个特征信息为飞行时间，同样忽略。
+
+    // 第七个特征信息：下标为 6 ，表示 origin 的所有取值。
+    paramCateFeaturesInfo += (6 -> mOrigin.size)
+
+    // 第八个特征信息：下标为 7， 表示 dest 的所有取值。
+    paramCateFeaturesInfo += (7 -> mDest.size)
+
+    // 分类的数量为 2，代表已延误航班和未延误航班。
+    val paramNumClasses = 2
+
+    // 下面的参数设置为经验值
+    val paramMaxDepth = 9
+    val paramMaxBins = 7000
+    val paramImpurity = "gini"
+
+    val flightDelayModel = DecisionTree.trainClassifier(trainingData, paramNumClasses, paramCateFeaturesInfo, paramImpurity, paramMaxDepth, paramMaxBins)
+
+    tmpDM = flightDelayModel.toDebugString
+    println(tmpDM)
+  }
+}
+
+def parseFields(input: String): Flight = {
+  val line = input.split(",")
+
+  // 针对可能出现的无效值“NA”进行过滤
+  var dayOfMonth = 0
+  if (line(0) != "NA") {
+    dayOfMonth = line(0).toInt
+  }
+  var dayOfWeek = 0
+  if (line(1) != "NA") {
+    dayOfWeek = line(1).toInt
+  }
+
+  var crsDepTime = 0.0
+  if (line(2) != "NA") {
+    crsDepTime = line(2).toDouble
+  }
+
+  var crsArrTime = 0.0
+  if (line(3) != "NA") {
+    crsArrTime = line(3).toDouble
+  }
+
+  var crsElapsedTime = 0.0
+  if (line(5) != "NA") {
+    crsElapsedTime = line(5).toDouble
+  }
+
+  var arrDelay = 0
+  if (line(8) != "NA") {
+    arrDelay = line(8).toInt
+  }
+  var depDelay = 0
+  if (line(9) != "NA") {
+    depDelay = line(9).toInt
+  }
+
+  // 根据延迟时间决定延迟标志是否为1
+  var delayFlag = 0
+  if (arrDelay > 30 || depDelay > 30) {
+    delayFlag = 1
+  }
+  Flight(dayOfMonth, dayOfWeek, crsDepTime, crsArrTime, line(4), crsElapsedTime, line(6), line(7), arrDelay, depDelay, delayFlag)
+}
+
+case class Flight(dayOfMonth: Int, dayOfWeek: Int, crsDepTime: Double, crsArrTime: Double, uniqueCarrier: String, crsElapsedTime: Double, origin: String, dest: String, arrDelay: Int, depDelay: Int, delayFlag: Int)
